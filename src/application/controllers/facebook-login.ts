477826c9b477eca89114451722115e9871760275
Mauricio-Natacci <mauricionatacci98@gmail.com>
2023-10-10 09:00:14 -0300

    feat: apply unauthorized helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,5 +1,5 @@
 import { RequiredFieldError, ServerError } from '@/application/errors'
-import { HttpResponse, badRequest } from '@/application/helpers'
+import { HttpResponse, badRequest, unauthorized } from '@/application/helpers'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 
@@ -12,21 +12,18 @@ export class FacebookLoginController {
         return badRequest(new RequiredFieldError('token'))
       }
 
-      const result = await this.facebookAuthentication.perform(httpRequest)
+      const accessToken = await this.facebookAuthentication.perform(httpRequest)
 
-      if (result instanceof AccessToken) {
+      if (accessToken instanceof AccessToken) {
         return {
           statusCode: 200,
           data: {
-            accessToken: result.value
+            accessToken: accessToken.value
           }
         }
       }
 
-      return {
-        statusCode: 401,
-        data: result
-      }
+      return unauthorized()
     } catch (error) {
       return {
         statusCode: 500,
