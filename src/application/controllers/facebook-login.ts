a2d14260cbae6bd2f0c631807dedd7260a54b8dc
Mauricio-Natacci <mauricionatacci98@gmail.com>
2023-10-10 12:47:23 -0300

    refactor: apply ValidationBuilder

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,5 +1,5 @@
 import { HttpResponse, badRequest, ok, serverError, unauthorized } from '@/application/helpers'
-import { RequiredStringValidator, ValidationComposite } from '@/application/validation'
+import { ValidationBuilder, ValidationComposite } from '@/application/validation'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 
@@ -33,9 +33,11 @@ export class FacebookLoginController {
   }
 
   private validate (httpRequest: HttpRequest): Error | undefined {
-    const validators = [
-      new RequiredStringValidator(httpRequest.token, 'token')
-    ]
+    const validators = ValidationBuilder
+      .of({ value: httpRequest.token, fieldName: 'token' })
+      .required()
+      .build()
+
     return new ValidationComposite(validators).validate()
   }
 }
