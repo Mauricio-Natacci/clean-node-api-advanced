0c3035bfcb11faf63eb297fa6d813ce6420b6da1
Mauricio-Natacci <mauricionatacci98@gmail.com>
2023-10-12 17:17:33 -0300

    test: ensure ExpressRouter calls handle with empty request

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -17,11 +17,22 @@ describe('ExpressRouter', () => {
     const { res } = getMockRes()
 
     const controller = mock<Controller>()
-
     const sut = new ExpressRouter(controller)
 
     await sut.adapt(req, res)
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+  })
+
+  it('should call handle with empty request', async () => {
+    const req = getMockReq()
+    const { res } = getMockRes()
+
+    const controller = mock<Controller>()
+    const sut = new ExpressRouter(controller)
+
+    await sut.adapt(req, res)
+
+    expect(controller.handle).toHaveBeenCalledWith({})
   })
 })
