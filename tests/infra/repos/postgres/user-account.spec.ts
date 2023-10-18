fd1e8d7c13123ab918e88894317b9727dcfa2dad
Mauricio-Natacci <mauricionatacci98@gmail.com>
2023-10-18 09:17:58 -0300

    refactor: improve architecture

--- tests/infra/repos/postgres/user-account.spec.ts
+++ tests/infra/repos/postgres/user-account.spec.ts
@@ -1,6 +1,6 @@
-import { PgUser } from '@/infra/postgres/entities'
-import { PgUserAccountRepository } from '@/infra/postgres/repos'
-import { makeFakeDb } from '@/tests/infra/postgres/mocks'
+import { PgUserAccountRepository } from '@/infra/repos/postgres'
+import { PgUser } from '@/infra/repos/postgres/entities'
+import { makeFakeDb } from '@/tests/infra/repos/postgres/mocks'
 import { IBackup } from 'pg-mem'
 import { Repository, getConnection, getRepository } from 'typeorm'
 
