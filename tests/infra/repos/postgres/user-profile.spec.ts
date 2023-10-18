03aa9a738aa88bf09b2ce81fd04cdbc3cd4990b0
Mauricio-Natacci <mauricionatacci98@gmail.com>
2023-10-18 14:36:58 -0300

    test: ensure PgUserProfileRepository returns undefined when user profile is not found

--- tests/infra/repos/postgres/user-profile.spec.ts
+++ tests/infra/repos/postgres/user-profile.spec.ts
@@ -47,5 +47,11 @@ describe('PgUserProfileRepository', () => {
 
       expect(userProfile?.name).toBe('any_name')
     })
+
+    it('should return undefined when user profile is not found', async () => {
+      const userProfile = await sut.load({ id: '1' })
+
+      expect(userProfile).toBeUndefined()
+    })
   })
 })
