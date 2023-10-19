afff3c99f52c89d61b0f553e9a452f0e0bef6420
Mauricio-Natacci <mauricionatacci98@gmail.com>
2023-10-19 10:39:12 -0300

    feat: add MaxFileSize validator

---
+++ tests/application/validation/max-file-size.spec.ts
@@ -0,0 +1,28 @@
+import { MaxFileSizeError } from '@/application/errors'
+import { MaxFileSize } from '@/application/validation'
+
+describe('MaxFileSize', () => {
+  it('should return MaxFileSizeError if value is invalid', () => {
+    const invalidBuffer = Buffer.from(new Array(6 * 1024 * 1024))
+    const sut = new MaxFileSize(5, invalidBuffer)
+    const error = sut.validate()
+
+    expect(error).toEqual(new MaxFileSizeError(5))
+  })
+
+  it('should return undefined if value is valid', () => {
+    const buffer = Buffer.from(new Array(4 * 1024 * 1024))
+    const sut = new MaxFileSize(5, buffer)
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+
+  it('should return undefined if value is valid', () => {
+    const buffer = Buffer.from(new Array(5 * 1024 * 1024))
+    const sut = new MaxFileSize(5, buffer)
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+})
