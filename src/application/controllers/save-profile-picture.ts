8c07f9155f01ac847bbb8be235c3e7ed32e7d341
Mauricio-Natacci <mauricionatacci98@gmail.com>
2023-10-19 08:43:16 -0300

    refactor: move production code to their own files

---
+++ src/application/controllers/save-profile-picture.ts
@@ -0,0 +1,30 @@
+import { InvalidMimeType, MaxFileSizeError, RequiredFieldError } from '@/application/errors'
+import { HttpResponse, badRequest, ok } from '@/application/helpers'
+import { ChangeProfilePicture } from '@/domain/use-cases'
+
+type HttpRequest = { file: { buffer: Buffer, mimeType: string }, userId: string }
+type Model = { initials?: string, pictureUrl?: string } | Error
+
+export class SaveProfilePictureController {
+  constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
+
+  async handle ({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
+    if (file === undefined || file === null || file.buffer.length === 0) {
+      return badRequest(new RequiredFieldError('file'))
+    }
+
+    const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg']
+
+    if (!allowedTypes.includes(file.mimeType)) {
+      return badRequest(new InvalidMimeType(['png, jpg, jpeg']))
+    }
+
+    if (file.buffer.length > 5 * 1024 * 1024) {
+      return badRequest(new MaxFileSizeError(5))
+    }
+
+    const data = await this.changeProfilePicture({ id: userId, file: file.buffer })
+
+    return ok(data)
+  }
+}
--- tests/application/controllers/save-profile-picture.spec.ts
+++ tests/application/controllers/save-profile-picture.spec.ts
@@ -1,47 +1,5 @@
-import { RequiredFieldError } from '@/application/errors'
-import { HttpResponse, badRequest, ok } from '@/application/helpers'
-import { ChangeProfilePicture } from '@/domain/use-cases'
-
-type HttpRequest = { file: { buffer: Buffer, mimeType: string }, userId: string }
-type Model = { initials?: string, pictureUrl?: string } | Error
-
-export class SaveProfilePictureController {
-  constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
-
-  async handle ({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
-    if (file === undefined || file === null || file.buffer.length === 0) {
-      return badRequest(new RequiredFieldError('file'))
-    }
-
-    const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg']
-
-    if (!allowedTypes.includes(file.mimeType)) {
-      return badRequest(new InvalidMimeType(['png, jpg, jpeg']))
-    }
-
-    if (file.buffer.length > 5 * 1024 * 1024) {
-      return badRequest(new MaxFileSizeError(5))
-    }
-
-    const data = await this.changeProfilePicture({ id: userId, file: file.buffer })
-
-    return ok(data)
-  }
-}
-
-export class InvalidMimeType extends Error {
-  constructor (allowed: string[]) {
-    super(`Invalid file type.Allowed types: ${allowed.join(', ')}`)
-    this.name = 'InvalidMimeType'
-  }
-}
-
-export class MaxFileSizeError extends Error {
-  constructor (maxSizeInMb: number) {
-    super(`File upload limit is ${maxSizeInMb}MB`)
-    this.name = 'MaxFileSizeError'
-  }
-}
+import { SaveProfilePictureController } from '@/application/controllers'
+import { InvalidMimeType, MaxFileSizeError, RequiredFieldError } from '@/application/errors'
 
 describe('SaveProfilePictureController', () => {
   let buffer: Buffer
