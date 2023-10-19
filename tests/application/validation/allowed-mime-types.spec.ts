1544513784870927e468e7c29a187d95ac8a8dcb
Mauricio-Natacci <mauricionatacci98@gmail.com>
2023-10-19 10:08:15 -0300

    test: ensure AllowedMimeTypes returns undefined if mime type is valid as jpg and jpeg

--- tests/application/validation/allowed-mime-types.spec.ts
+++ tests/application/validation/allowed-mime-types.spec.ts
@@ -22,8 +22,22 @@ describe('AllowedMimeTypes', () => {
     expect(error).toEqual(new InvalidMimeTypeError(['png']))
   })
 
-  it('should return undefined if mime type is allowed', () => {
+  it('should return undefined if mime type is allowed with png', () => {
     const sut = new AllowedMimeTypes(['png'], 'image/png')
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+
+  it('should return undefined if mime type is allowed with jpg', () => {
+    const sut = new AllowedMimeTypes(['jpg'], 'image/jpg')
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+
+  it('should return undefined if mime type is allowed with jpeg', () => {
+    const sut = new AllowedMimeTypes(['jpeg'], 'image/jpeg')
     const error = sut.validate()
 
     expect(error).toBeUndefined()
