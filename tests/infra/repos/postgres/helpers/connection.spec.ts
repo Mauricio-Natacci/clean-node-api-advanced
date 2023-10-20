0a9a2f83e2077826bb1733699565b3036cab7171
Mauricio-Natacci <mauricionatacci98@gmail.com>
2023-10-20 16:39:44 -0300

    test: ensure PgConnection is a singleton

---
+++ tests/infra/repos/postgres/helpers/connection.spec.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class PgConnection {
+  private static instance?: PgConnection
+
+  private constructor () {}
+
+  static getInstance (): PgConnection {
+    if (PgConnection.instance === undefined) {
+      PgConnection.instance = new PgConnection()
+    }
+
+    return PgConnection.instance
+  }
+}
+
+describe('PgConnection', () => {
+  it('should have only one instance', async () => {
+    const sut = PgConnection.getInstance()
+    const sut2 = PgConnection.getInstance()
+
+    expect(sut).toBe(sut2)
+  })
+})
